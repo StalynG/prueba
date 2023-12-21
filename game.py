6dac73e3d81c23c19dbb2837882fbd1fcb56b00e
StalynG <153782308+StalynG@users.noreply.github.com>
2023-12-20 22:56:07 -0500

    Cambio color azul

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ high_score = 0
 # Set up the screen
 wn = turtle.Screen()
 wn.title("Snake Game")
-wn.bgcolor("brown")
+wn.bgcolor("blue")
 wn.setup(width=600, height=600)
 wn.tracer(0) # Turns off the screen updates
 
@@ -179,4 +179,3 @@ while True:
 
     time.sleep(delay)
 
-wn.mainloop()
